3e9e3920c1a31e119e7806f44923666faca8b624
Caetanoog18 <caetanoog@outlook.com>
2024-11-21 23:13:18 -0300

    pre_processing

--- pre_processing/main.py
+++ pre_processing/main.py
@@ -2,7 +2,7 @@ from tensorflow.python.ops.summary_ops_v2 import graph
 
 from pre_processing.census_database import CensusDatabase
 from pre_processing.census_graph import CensusGraph
-from pre_processing.handling_inconsistent_values import InconsistentValues, Census
+from pre_processing.pre_processing_database import InconsistentValues, Census
 from pre_processing.credit_risk_graph import *
 from pre_processing.census_database import *
 
@@ -14,26 +14,31 @@ file_path_census = "/home/gabriel-caetano/Desktop/Tensorflow/pre_processing/data
 # graphs.generate_dynamic_graph("dynamic_1")
 # graphs.historian_chart_age("historian_age_1")
 #
-# inconsistent_values = InconsistentValues(file_path_credit)
-# inconsistent_values.inconsistent_values()
+credit = InconsistentValues(file_path_credit)
+credit.inconsistent_values()
 #
 # inconsistent_values.generate_dynamic_graph("dynamic_1")
 # inconsistent_values.historian_chart_age("historian_age_2")
 #
-# inconsistent_values.missing_values()
+credit.missing_values()
 #
-# inconsistent_values.division_predictors_class()
+credit.division_predictors_class()
+
+credit.training_credit_database()
+credit.save_variables()
 
 census_database = CensusDatabase(file_path_census)
 census_database.statistics_census()
-
-census_graph = CensusGraph(file_path_census)
-census_graph.view_census_data("census_graph_income")
-census_graph.historian()
-
-census_graph = Census(file_path_census)
-census_graph.division_predictors_class_census()
-census_graph.label_encoder()
-census_graph.one_hot_encoder()
-census_graph.scaling_values()
+#
+# census_graph = CensusGraph(file_path_census)
+# census_graph.view_census_data("census_graph_income")
+# census_graph.historian()
+#
+census = Census(file_path_census)
+census.division_predictors_class_census()
+census.label_encoder()
+census.one_hot_encoder()
+census.scaling_values()
+census.training_census_database()
+census.save_variables()
 
